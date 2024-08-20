e4ea6351253a16bdd9a6a0a3fcc0d2b981460ccc
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-19 23:25:26 -0300

    Aula Ola, mundo!

---
+++ AulasJava/src/aula1_helloworld/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package aula1_helloworld;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
