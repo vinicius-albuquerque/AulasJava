f412c3aaa51132da6e1e86de19e53a6b9e5a2cfa
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-19 23:33:27 -0300

    Escrevendo no console

--- AulasJava/src/aula1_helloworld/PrimeiroPrograma.java
+++ AulasJava/src/aula1_helloworld/PrimeiroPrograma.java
@@ -5,6 +5,9 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		// escrevendo na mesma linha
+		System.out.print("Olá, ");
+		System.out.print("Mundo");
 	}
 
 }
