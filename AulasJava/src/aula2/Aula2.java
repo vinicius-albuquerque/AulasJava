cd8be645db6d1ee1e0288e3c2a559e00781f7100
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-20 00:07:14 -0300

    tipo char e numeros

---
+++ AulasJava/src/aula2/Aula2.java
@@ -0,0 +1,29 @@
+package aula2;
+
+public class Aula2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		variaveis();
+	}
+	
+	static void variaveis() {
+		//iniciando variavel
+		int idade;
+		idade = 18;
+		
+		//declaracao direta
+		float altura = 1.80f;
+		
+		System.out.print("A pessoa tem " + idade + " anos");
+		System.out.println(" e " + altura + " metros de altura.");
+		
+		
+		// tipo char (aceita apenas uma letra)
+		char inicial = 'C';
+		System.out.println(inicial);
+		
+		
+	}
+
+}
