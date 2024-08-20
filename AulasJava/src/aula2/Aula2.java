82cdd70f1f5a2acffc9109463b8acd77078aaafd
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-20 00:40:16 -0300

    saidas de dados diferentes, prints formatados de numeros

--- AulasJava/src/aula2/Aula2.java
+++ AulasJava/src/aula2/Aula2.java
@@ -5,6 +5,7 @@ public class Aula2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		variaveis();
+		imprimindo();
 	}
 	
 	static void variaveis() {
@@ -19,7 +20,7 @@ public class Aula2 {
 		System.out.println(" e " + altura + " metros de altura.");
 		
 		
-		// tipo char (aceita apenas uma letra)
+		// tipo char (aceita apenas uma letra e aspas simples)
 		char inicial = 'C';
 		System.out.println(inicial);
 		
@@ -30,6 +31,37 @@ public class Aula2 {
 		System.out.println(casado);
 		System.out.print(cr7);
 		
+		//
+	}
+	
+	static void imprimindo() {
+		//imprimindo de outras formas
+		//2024-08-18
+		String nome = "W. Rato";
+		float sofaScore = 7.2f;
+		System.out.print("\nJogador: " + nome);
+		System.out.print(" | nota Sofascore: " + sofaScore);
+		
+		
+		//Usando print f
+		nome = "Rafael";
+		sofaScore = 7f;
+		System.out.print("\nJogador: " + nome);
+		System.out.printf(" | nota Sofascore: %f", sofaScore);
+		
+		
+		//Usando print f controlando numero de casas decimais
+		nome = "Luciano";
+		sofaScore = 6.9f;	
+		System.out.print("\nJogador: " + nome);
+		System.out.printf(" | nota Sofascore: %.2f", sofaScore);
+		
+		
+		//Usando print format controlando numero de casas decimais
+		nome = "Calleri";
+		sofaScore = 6.7f;	
+		System.out.print("\nJogador: " + nome);
+		System.out.format(" | nota Sofascore: %.1f", sofaScore);
 	}
 
 }
