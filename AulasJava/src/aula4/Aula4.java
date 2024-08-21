7ad0a1cb155a7f8ce4d7775752a06ddf2f378489
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-21 01:39:26 -0300

    operador ternario

--- AulasJava/src/aula4/Aula4.java
+++ AulasJava/src/aula4/Aula4.java
@@ -1,10 +1,24 @@
 package aula4;
+import java.util.Scanner;
 
 public class Aula4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Vamos algumas operações lógicas o/");
+		System.out.print("Insira um número: ");
+		int a = in.nextInt();
+		System.out.print("Agora outro número: ");
+		int b = in.nextInt();
+		
+		operadorTernario(a,b);
 	}
 
+	static void operadorTernario(int a, int b) {
+		int maior;
+		System.out.println("\nUtilizando operador ternário");
+		maior = (a>b)?a:b;
+		System.out.println("Maior: " + maior);
+	}
 }
