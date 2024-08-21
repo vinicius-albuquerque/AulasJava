a3ec9a7d3338d9a5f2a37ad658a72c4c738c519a
Vinicius <dealbuquerquevinicius@gmail.com>
2024-08-21 01:58:48 -0300

    comparacao de string e objs

--- AulasJava/src/aula4/Aula4.java
+++ AulasJava/src/aula4/Aula4.java
@@ -13,6 +13,7 @@ public class Aula4 {
 		int b = in.nextInt();
 		
 		operadorTernario(a,b);
+		comparacaoString();
 	}
 
 	static void operadorTernario(int a, int b) {
@@ -20,5 +21,24 @@ public class Aula4 {
 		System.out.println("\nUtilizando operador ternário");
 		maior = (a>b)?a:b;
 		System.out.println("Maior: " + maior);
+	}
+	
+	static void comparacaoString() {
+		String animal1 = "Rato";
+		String animal2 = "Rato";
+		String animal3 = new String("Rato");
+		String comp1,comp2,comp3;
+		
+		System.out.println("\nAnimal1 = "+ animal1 + "\nAnimal2 = "+ animal2 + "\nAnimal3 = "+ animal3);
+		comp1 = (animal1==animal2)?"igual":"diferente";
+		comp2 = (animal1==animal3)?"igual":"diferente";
+		System.out.println("Comparação 1: " + comp1);
+		System.out.println("Comparação 2: " + comp2);
+		
+		System.out.println("A comparacao entre animal1 e animal3 retorna diferente devido ao metodo de criacao dessas variaveis");
+		System.out.println("Aqui devemos utilizar a funcao equals das string para verificar se ambas possuem o mesmo conteudo");
+		comp3 = (animal1.equals(animal3))?"igual":"diferente";
+		System.out.println("Comparação 3: " + comp3);
+		System.out.println("A mesma logica se dá a todos os objetos e classes involucros (new Integer(), new Double(), New String() etc.");
 	}
 }
